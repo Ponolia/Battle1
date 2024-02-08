041eb85849c21319fcefb7d63c205232171e7c57
Jiwon <morex1566@gmail.com>
2024-02-08 16:20:23 +0900

    plus
    plus

---
+++ My project/Assets/Scirpts/Player/PlayerMove.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMove : MonoBehaviour
+{
+    
+    public void MovetoPos(Vector3 pos, UnityAction done)
+    {
+        StopAllCoroutines();
+        StartCoroutine(MovingPos(pos, done));
+    }
+    protected IEnumerator MovingPos(Vector3 pos, UnityAction done)
+    {
+        
+        Vector3 dir = pos - transform.position;
+        float dist = dir.magnitude;
+        dir.Normalize();
+
+        while (true)
+        {
+            float delta = 2.0f * Time.deltaTime;
+
+            if (delta > dist) delta = dist;
+            dist -= delta;
+
+            transform.Translate(dir * delta, Space.World);
+            yield return null;
+        }
+        done?.Invoke();
+    }
+    protected IEnumerator Rotating()
+    {
+        yield return null;
+    }
+}
