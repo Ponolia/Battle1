ffdd92f14787d1e0aeda19fac8bdde15701bbeb0
Jiwon <morex1566@gmail.com>
2024-02-08 16:17:09 +0900

    atents
    player click move ok,
    회전 추가 하고 애니메이션 넣기

---
+++ My project/Assets/Scirpts/Picking.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Picking : PlayerMove
+{
+    public LayerMask clickMask;
+    public UnityEvent<Vector3> clickAct;
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray,out RaycastHit hit, Mathf.Infinity, clickMask))
+            {
+                clickAct?.Invoke(hit.point);
+            }
+        }
+    }
+}
